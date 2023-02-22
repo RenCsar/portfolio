ad349bc25a463ef8ad50205810d214c20130bc3d
Renan Cesar <cesar.renan@hotmail.com>
2023-02-22 15:49:45 -0300

    Estilizando contato

--- src/components/contato/Contato.Styled.ts
+++ src/components/contato/Contato.Styled.ts
@@ -1,16 +1,39 @@
 import styled from "styled-components";
 
+const gradiente = `
+    background: -webkit-linear-gradient(#15cdcb, #5280e2);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`
+
 export const Container = styled.section`
         padding: var(--padding-central);
         display: flex;
         align-items: center;
         flex-direction: column;
-        /* margin-top: 100px; */
+        height: 600px;
+        gap: 20px;
+        margin-top: 50px;
 
         > h1 {
-            color: var(--verde);
-            font-size: 32px;
-            border-bottom: 2px solid var(--verde);
             margin-bottom: 20px;
+            font-size: 32px;
+            border-bottom-width: 2px;
+            border-bottom-style: solid;
+            border-image: -webkit-linear-gradient(#15cdcb, #5280e2) 1;
+            ${gradiente};
+        }
+        
+        > p{
+            color: var(--branco);
+        }
+        
+        & :nth-child(2) {
+            width: 600px;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        & :nth-child(4) {
+            ${gradiente};
         }
 `
--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -1,9 +1,19 @@
+import Button from '../button/Button'
 import { Container } from './Contato.Styled'
 
 const Contato = () => {
+
+  const linkContato: string = 'https://api.whatsapp.com/send?phone=5521990926768&text=sua%20mensagem';
+
   return (
     <Container>
       <h1>Contato</h1>
+      <p>
+      Se você gostou do meu trabalho e quer que eu tire sua ideia do papel ou faça parte do seu time, entre em contato comigo! 
+      </p>
+      <Button texto='Entre em contato' link={linkContato}/>
+      <p>Ou</p>
+      <p>Baixar meu currículo completo</p>
     </Container>
   )
 }
