a5e8f98c75d633081dad6d5c3e2697c7047f94c2
Renan Cesar <cesar.renan@hotmail.com>
2023-06-20 17:26:25 -0300

    Ajustando delay da animação das seções 'sobre mim' e 'contato'

--- src/components/contato/Contato.tsx
+++ src/components/contato/Contato.tsx
@@ -11,23 +11,23 @@ const Contato = () => {
   return (
     <Element name='contato'>
       <Container>
-        <Fade direction="down" delay={500} duration={900} triggerOnce cascade>
+        <Fade direction="down" delay={100} duration={400} triggerOnce cascade>
           <h2>Contato</h2>
         </Fade>
         <div className='content'>
-          <Fade direction="down" delay={500} duration={900} triggerOnce>
+          <Fade direction="down" delay={100} duration={400} triggerOnce>
             <div>
               <h3>Vamos trabalhar juntos?</h3>
             </div>
           </Fade>
-          <Fade direction="down" delay={500} duration={900} triggerOnce>
+          <Fade direction="down" delay={100} duration={400} triggerOnce>
             <div className='text-container'>
               <p>
                 Se você gostou do meu trabalho e quer que eu tire sua ideia do papel ou faça parte do seu time, entre em contato comigo!
               </p>
             </div>
           </Fade>
-          <Fade direction="down" delay={500} duration={900} triggerOnce>
+          <Fade direction="down" delay={100} duration={400} triggerOnce>
             <Button texto='Entre em contato' link={linkContato} />
             <p>Ou</p>
             <ButtonCvc texto={'Baixar meu currículo completo'} link={LinkCVC} />
--- src/components/sobreMim/SobreMin.tsx
+++ src/components/sobreMim/SobreMin.tsx
@@ -12,33 +12,23 @@ export const SobreMin = () => {
         <Element name='sobremim'>
             <Container id='sobremin'>
                 <div className='text-container'>
-                    <Fade direction="down" delay={200} duration={500} triggerOnce cascade>
+                    <Fade direction="down" delay={100} duration={400} triggerOnce cascade>
                         <p>Olá,</p>
-                    {/* </Fade>
-                    <Fade direction="down" delay={400} duration={900} triggerOnce> */}
                         <h1>Eu sou o Renan Cesar</h1>
-                    {/* </Fade> */}
-                    {/* <Fade direction="down" delay={400} duration={900} triggerOnce> */}
                         <p>Desenvolvedor Front-end</p>
-                    {/* </Fade> */}
-                    {/* <Fade direction="down" delay={200} duration={900} triggerOnce> */}
                         <div className='descricao'>
                             <p>Sou formado em análise e Desenvolvimento de sistemas e estou me dedicando as tecnologias frontend. Adoro aprender novas tecnologias e trabalhar em equipe.</p>
                         </div>
-                    {/* </Fade> */}
-                    {/* <Fade direction="down" delay={100} duration={900} triggerOnce> */}
                         <div className='button-container'>
                             <Button texto="Entre em contato" link={linkContato} />
                         </div>
                     </Fade>
                 </div>
-                <Fade direction="down" delay={900} duration={900} triggerOnce cascade>
-                    <div className='img-container'>
-                        <div className='foto-container'>
-                            <img src={Perfil} alt="imagem de perfil" />
-                        </div>
+                <div className='img-container'>
+                    <div className='foto-container'>
+                        <img src={Perfil} alt="imagem de perfil" />
                     </div>
-                </Fade>
+                </div>
             </Container>
         </Element>
     )
